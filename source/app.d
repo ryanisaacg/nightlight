0953d9b741d04b2d91ec2ec8b9d1e4b0e52aaf45
Ryan Goldstein <ryanisaacg@gmail.com>
2016-10-01 21:27:20 -0400

    Added a window that opens to the project

--- source/app.d
+++ source/app.d
@@ -1,7 +1,17 @@
+import derelict.sdl2.sdl;
 import std.stdio;
-import arcade.source.geom;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+void sdl_check(bool check) {
+	if(check)
+		writefln("SDL Error: %s\n", SDL_GetError());
+}
+
+void main() {
+    DerelictSDL2.load();
+    sdl_check(SDL_Init(SDL_INIT_VIDEO) < 0);
+    SDL_Window *window = SDL_CreateWindow("Project Nightlight", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    sdl_check(window == null);
+    SDL_Delay(1000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 }
